d673a1a662679ec2cbaca4e77393ed7867be8690
Mrityunjay20 <mrityunjay.shrivastava20@gmail.com>
2024-03-30 20:03:11 +0530

    updated treatment description

--- src/app/components/NavbarWithMegaMenu.js
+++ src/app/components/NavbarWithMegaMenu.js
@@ -42,52 +42,52 @@ import { Alert } from "@material-tailwind/react";
 const navListMenuItems = [
   {
     title: "Angiography",
-    description: "Find the perfect solution for your needs.",
+    description: "Experience our unparalleled precision of Angiography: A sophisticated medical imaging solution, illuminating vascular intricacies with unrivaled clarity and detail.",
     icon: HeartIcon,
   },
   {
     title: "Angioplasty (PICA)",
-    description: "Meet and learn about our dedication",
+    description: "Using advanced imaging techniques and specialized equipment, our experienced medical team precisely identifies areas of concern within the arteries and employs innovative techniques to widen the narrowed segments, enhancing blood flow and alleviating symptoms associated with vascular disease.",
     icon: CogIcon,
   },
   {
     title: "Pacemaker Implantation",
-    description: "Find the perfect solution for your needs.",
+    description: "We utilize the latest advancements in cardiac technology to ensure the highest level of precision and efficacy during placemaker implantation procedures. Our state-of-the-art equipment and implantable devices are designed to deliver reliable and consistent performance, providing patients with the peace of mind they deserve.",
     icon: RssIcon,
   },
   {
     title: "Implantable Cardioverter Defibrillator",
-    description: "Reach out to us for assistance or inquiries",
+    description: "We prioritize the comfort and well-being of our patients throughout their ICD implantation journey. From thorough pre-procedural assessments to comprehensive post-implantation support and education, our team is dedicated to providing compassionate care and empowering patients to manage their cardiac health effectively.",
     icon: BoltIcon,
   },
   {
     title: "CRT-D /CRT-P Implantation",
-    description: "Find the perfect solution for your needs.",
+    description: "Discover refined cardiac support through our state-of-the-art CRT-D/CRT-P implantation services. Our seasoned specialists deliver tailored care to optimize cardiac function and enhance the quality of life for those grappling with heart failure and conduction anomalies. Reach out to us for further insights.",
     icon: ExclamationTriangleIcon,
   },
   {
     title: "Balloon Mitral Valvotomy (PTMC)",
-    description: "Read insightful articles, tips, and expert opinions.",
+    description: "Embark on a journey of compassionate cardiac care with our Balloon Mitral Valvotomy (PTMC) services. We offer a personalized approach aimed at restoring mitral valve function and nurturing overall cardiac well-being. Reach out to us today to begin your path towards improved heart health with understanding and support.",
     icon: ChevronDoubleRightIcon,
   },
   {
     title: "Post Bypass Angioplasties",
-    description: "Find the perfect solution for your needs.",
+    description: "Explore the specialized care of our Post Bypass Angioplasty treatment, to guiding your recovery journey with precision and compassion. From personalized support to vigilant monitoring, we are committed to nurturing your cardiac well-being. Take the next step towards a revitalized heart by connecting with us today for unparalleled expertise and care.",
     icon: AdjustmentsHorizontalIcon,
   },
   {
     title: "Renal Angioplasty",
-    description: "Explore limited-time deals and bundles",
+    description: "We offer meticulous evaluation and tailored interventions aimed at optimizing renal function. With an emphasis on medical precision and cutting-edge techniques, our Renal Angioplasty treatment ensures comprehensive management for each patient's specific renal health needs.",
     icon: ArrowUpOnSquareStackIcon,
   },
   {
     title: "CHIP Angioplasties",
-    description: "Explore limited-time deals and bundles",
+    description: "With a focus on precision and innovation, our CHIP Angioplasty treatment ensures thorough management and optimal outcomes for your heart's well-being. Experience the difference in cardiac care, where excellence meets innovation to safeguard your heart's health.",
     icon: CogIcon,
   },
   {
     title: "ECG ECHO",
-    description: "Explore limited-time deals and bundles",
+    description: "We offer comprehensive evaluations utilizing state-of-the-art technology to unveil insights into cardiac function and rhythm. Experience the precision of medical diagnostics as we delve into the intricacies of your heart health, guiding you towards informed decisions and optimal wellness.",
     icon: TagIcon,
   },
 ];
